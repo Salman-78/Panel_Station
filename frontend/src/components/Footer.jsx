f0ac512065ec7b21b40d2822dd626c0aa4fc351d
Salman <smd397452@gmail.com>
2025-06-29 15:59:47 +0530

    footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import "./Footer.css"
 export const Footer = () => {
     return(
         <footer>
-            <p>@thapatechnical 2025</p>
+            <p>@Mern_Series 2025</p>
         </footer>
     );
 }
